759c1863632bbc43d22262b2490a449bca3b1092
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-02-16 21:09:55 -0600

    Started making controllers

---
+++ app/Http/Controllers/SubbredditController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App;
+
+class SubbredditController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return App\Subbreddit::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Show all the comments of all of the posts that belong to the subbreddit with the id of $id
+        // Eloquent sees 'posts.comments' as the relationships of posts() and comments() within Subbreddits and Posts, respectively
+        return App\Subbreddit::with('posts.comments')->find($id);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::resource('subbreddit', 'SubbredditController');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
