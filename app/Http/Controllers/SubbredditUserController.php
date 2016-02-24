d8ab5e21b6dbff6425cf577e6926880d5e1bcb95
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-02-23 21:22:26 -0600

    Created SubbredditsUserController

---
+++ app/Http/Controllers/SubbredditUserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App;
+
+class SubbredditUserController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = \Auth::user();
+        $subbreddit = App\Subbreddit::find($request->subbreddit_id);
+        $user->subscribedSubbreddits()->attach($subbreddit);
+
+        // Because we have defined the relationship, the attach assumes that the ID we are giving it is the subbreddit's
+        // So instead of the above 2 lines you could just say...
+        // $user->subscribedSubbreddts()->attach($request->subbreddit_id);
+
+        return 'true';
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
