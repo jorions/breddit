f7351074f3791c1d7e97fb1c131d959a45c307c2
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-02-23 20:20:21 -0600

    Updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,13 +24,18 @@
 |
 */
 
-// These are the routes that can be accessed without being logged in
-Route::group(['middleware' => ['web']], function () {
 
-    // Moved from outside the middleware
+Route::group(['middleware' => 'web'], function () {
+
     Route::get('/', function () {
         return view('welcome');
     });
+
+    // Generates the routes for your authentication
+    Route::auth();
+
+    // Automatically generated
+    Route::get('/home', 'HomeController@index');
 
     // index() and show() can be seen without being logged in
     Route::resource('subbreddit', 'SubbredditsController', [
@@ -49,31 +54,24 @@ Route::group(['middleware' => ['web']], function () {
         'only' => ['index', 'show']
     ]);
 
+    // These are the routes that can only be accessed once you are logged in
+    Route::group(['middleware' => 'auth'], function () {
 
-});
+        Route::resource('subbreddit', 'SubbredditsController', [
+            'only' => ['store', 'update', 'destroy']
+        ]);
 
-// These are the routes that can only be accessed once you are logged in
-Route::group(['middleware' => 'web'], function () {
+        Route::resource('user', 'UsersController', [
+            'only' => ['store', 'update', 'destroy']
+        ]);
 
-    // Says ????????
-    Route::auth();
+        Route::resource('post', 'PostsController', [
+            'only' => ['store', 'update', 'destroy']
+        ]);
 
-    Route::get('/home', 'HomeController@index');
+        Route::resource('comment', 'CommentsController', [
+            'only' => ['store', 'update', 'destroy']
+        ]);
 
-    Route::resource('subbreddit', 'SubbredditsController', [
-        'except' => ['create', 'edit']
-    ]);
-
-    Route::resource('user', 'UsersController', [
-        'except' => ['create', 'edit']
-    ]);
-
-    Route::resource('post', 'PostsController', [
-        'except' => ['create', 'edit']
-    ]);
-
-    Route::resource('comment', 'CommentsController', [
-        'except' => ['create', 'edit']
-    ]);
-
+    });
 });
