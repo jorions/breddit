bbc5487c22082d9336f57d69f7453e01612f8a33
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-03-01 20:38:55 -0600

    Moved routes into an 'api' group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -37,41 +37,47 @@ Route::group(['middleware' => 'web'], function () {
     // Automatically generated
     Route::get('/home', 'HomeController@index');
 
-    // index() and show() can be seen without being logged in
-    Route::resource('subbreddits', 'SubbredditsController', [
-        'only' => ['index', 'show']
-    ]);
+    // This will put "api/" before every route inside of the group. We do this because our routes are actually part of our
+    // backend API, not part of how the user will interact with the page on the frontend. We want a user to be able to visit
+    // /subbreddit and have it pull in views etc, not just be our API calls.
+    Route::group(['prefix' => 'api'], function() {
 
-    Route::resource('users', 'UsersController', [
-        'only' => ['index', 'show']
-    ]);
-
-    Route::resource('posts', 'PostsController', [
-        'only' => ['index', 'show']
-    ]);
-
-    Route::resource('comments', 'CommentsController', [
-        'only' => ['index', 'show']
-    ]);
-
-    // These are the routes that can only be accessed once you are logged in
-    Route::group(['middleware' => 'auth'], function () {
-
+        // index() and show() can be seen without being logged in
         Route::resource('subbreddits', 'SubbredditsController', [
-            'only' => ['store', 'update', 'destroy']
+            'only' => ['index', 'show']
         ]);
 
         Route::resource('users', 'UsersController', [
-            'only' => ['store', 'update', 'destroy']
+            'only' => ['index', 'show']
         ]);
 
         Route::resource('posts', 'PostsController', [
-            'only' => ['store', 'update', 'destroy']
+            'only' => ['index', 'show']
         ]);
 
         Route::resource('comments', 'CommentsController', [
-            'only' => ['store', 'update', 'destroy']
+            'only' => ['index', 'show']
         ]);
 
+        // These are the routes that can only be accessed once you are logged in
+        Route::group(['middleware' => 'auth'], function () {
+
+            Route::resource('subbreddits', 'SubbredditsController', [
+                'only' => ['store', 'update', 'destroy']
+            ]);
+
+            Route::resource('users', 'UsersController', [
+                'only' => ['store', 'update', 'destroy']
+            ]);
+
+            Route::resource('posts', 'PostsController', [
+                'only' => ['store', 'update', 'destroy']
+            ]);
+
+            Route::resource('comments', 'CommentsController', [
+                'only' => ['store', 'update', 'destroy']
+            ]);
+
+        });
     });
 });
