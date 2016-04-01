22fead972546cb6dac4e8dabc75d3534544eeb13
Jared Selcoe <jared.orion.selcoe@gmail.com>
2016-03-31 19:45:41 -0500

    Created gulpfile and started working on it

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,59 @@
-var elixir = require('laravel-elixir');
+'use strict';
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+// require() is not part of javascript - it is node.js. It is just like a php require - it inserts the code from the specified
+// file or package. If you pass in a whole file path (like "require('./gulp')") it will look for that as a file. If you
+// just pass in a name like we have here, it will treat it as a package to search for
+// ###################################################################################################################
+// require() automatically looks at the folder that the current file lives in for a file of the given name (it assumes
+// the file ends with ".js"). If the file is not there, it checks the package.json at that level, and if the specified name
+// is there, it then searches the folder node_modules for a folder with the given name, and then searches that folder for
+// its package.json, and then looks for the "main" or "files" section of that .json, and that points to another file, which
+// in this case is index.js. THAT is the code that becomes included.
+// ###################################################################################################################
+// To make this work, in the terminal, from the home directory of the project, we need to run "npm install" which will
+// create the node_modules folder in this project, and automatically installs gulp among many other things. Then we have
+// to run "npm install --global gulp" to make gulp install globally. We have to specify 'gulp' because globally there isn't
+// a package.json for it to look at. May have to specify "sudo" before the command to make it work
+var gulp = require('gulp');
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+
+// We updated our dependencies and installed this package by going into the terminal from our home directory and typing
+// "npm install browserify --save-dev" which will install the browserify package and then uses the "--save-dev" portion to
+// add it as a dev dependency. It is a dev dependency instead of a normal dependency because we are only using it for
+// development to organize our work. Our app is not going to be inherently reliant on it
+var browserify = require('browserify');
+
+
+// "npm install vinyl-source-stream --save-dev"
+var source = require('vinyl-source-stream');
+
+// "npm install vinyl-buffer --save-dev"
+var buffer = require('vinyl-buffer');
+
+
+// Creates a task called 'default'. Now, in the terminal when we are inside the home directory, we could type 'gulp default'
+// which will log "hello" to the terminal. 'default' is special in that if we just type "gulp" in the terminal, 'default'
+// runs automatically. No other task name does this.
+//gulp.task('default', function() {
+//    console.log("hello");
+//});
+
+
+// The task has a syntax specified on the browserify.org page. It has 2 dependencies (defined above) that we had to
+// install in addition to the other dependencies: "npm install vinyl-source-stream --save-dev" and
+// "npm install vinyl-buffer --save-dev"
+// ###################################################################################################################
+// pipe() takes the output of the previous command, and then passes it into the next command
+// ###################################################################################################################
+// buffer() takes a file as input, and outputs it as a vinyl file
+// ###################################################################################################################
+// This will take the 'app.js' file, 'browserify' it, and the put that output into a file called 'bundle.js' in the
+// 'public/js/' folder
+gulp.task('default', function() {
+    return browserify({
+       entries: ['public/js/app.js']
+    }).bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('public/js/'));
 });
